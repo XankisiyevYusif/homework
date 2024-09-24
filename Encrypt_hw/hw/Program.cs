c7931ffee956aed0ad2412a0d59b9b8bfdc67bae
XankisiyevYusif <69992029+XankisiyevYusif@users.noreply.github.com>
2024-09-24 11:42:21 +0400

    Add files via upload

---
+++ Encrypt_hw/hw/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Threading.Channels;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Fayilin youlunu daxil edin: ");
+        string filePath = Console.ReadLine();
+
+        if(File.Exists(filePath))
+        {
+            string fileContent = File.ReadAllText(filePath);
+
+            Console.WriteLine("Acari daxil edin: ");
+            int key = Convert.ToInt32(Console.ReadLine());    
+            string encryptData = EncryptData(fileContent, key);
+            
+            File.WriteAllText(filePath, encryptData);
+            Console.WriteLine("Fayl sifrelendi");
+        }
+        else
+        {
+            {
+                Console.WriteLine("Bele fayl movcud deyil!");
+            }
+        }
+
+        static string EncryptData(string data, int key)
+        {
+            StringBuilder encrypted_file = new StringBuilder(); 
+
+            foreach(char c in data)
+            {
+                encrypted_file.Append((char)(c ^ key));
+            }
+
+            return encrypted_file.ToString();
+        }
+    }
+}
