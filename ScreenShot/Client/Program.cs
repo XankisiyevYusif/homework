098eca6f83051811443df99ad843fdaafa205096
XankisiyevYusif <69992029+XankisiyevYusif@users.noreply.github.com>
2024-10-13 20:13:34 +0300

    Add files via upload

---
+++ ScreenShot/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main()
+    {
+        string serverIp = "192.168.0.109";
+        int port = 27001;
+
+        while (true)
+        {
+            using (TcpClient client = new TcpClient(serverIp, port))
+            using (NetworkStream stream = client.GetStream())
+            {
+                int widh = 1920;
+                int height = 1080;
+
+                using (Bitmap bmp = new Bitmap(widh, height))
+                {
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.CopyFromScreen(0, 0, 0, 0, new Size(widh, height));
+                    }
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        bmp.Save(ms, ImageFormat.Jpeg);
+                        byte[] imageBytes = ms.ToArray();
+
+                        stream.Write(imageBytes, 0, imageBytes.Length);
+                    }
+
+                    Console.WriteLine("Picture sent succeesful!");
+                }
+            }
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ ScreenShot/ScreenShot/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main()
+    {
+        int port = 27001;
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("Server Started");
+
+        while (true)
+        {
+            try
+            {
+                using (TcpClient client = server.AcceptTcpClient())
+                using (NetworkStream stream = client.GetStream())
+                {
+                    string clientId = client.Client.RemoteEndPoint.ToString();
+                    string sanitizedClientId = clientId.Replace(":", "_").Replace(".", "_");
+
+                    string clientFolder = Path.Combine("Screenshots", sanitizedClientId);
+
+                    if (!Directory.Exists(clientFolder))
+                    {
+                        Directory.CreateDirectory(clientFolder);
+                    }
+
+                    byte[] buffer = new byte[1024 * 1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    string fileName = Path.Combine(clientFolder, $"screenshot_{DateTime.Now:yyyyMMdd_HHmmss}.jpg");
+                    using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                    {
+                        fs.Write(buffer, 0, bytesRead);
+                    }
+
+                    Console.WriteLine($"Picture downloaded: {fileName}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
